39705a1e5d22e777fe7eb673c6425d99a78d59ff
Mr.Xbott <mr.xbott@gmail.com>
2025-05-18 19:48:46 +0300

    change password form for admin (with WTForms)

--- shop_bot/admin/routes/auth.py
+++ shop_bot/admin/routes/auth.py
@@ -1,11 +1,12 @@
 from flask import render_template, request, redirect, url_for, flash
-from flask_login import login_user, logout_user, login_required, current_user
-from werkzeug.security import check_password_hash
+from flask_login import login_user, logout_user, current_user
+from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy import select
 
 from . import routes_bp
 from db.models import Admin
 from db.init import sync_session
+from forms.passwords import ChangePasswordForm
 
 @routes_bp.route('/login/', methods=['GET', 'POST'])
 def login():
@@ -22,7 +23,25 @@ def login():
     return render_template('login.html')
 
 @routes_bp.route('/logout')
-@login_required
 def logout():
     logout_user()
     return redirect(url_for('admin.login'))
+
+@routes_bp.route('/change-password/', methods=['GET', 'POST'])
+def change_password():
+    form = ChangePasswordForm()
+
+    if form.validate_on_submit():
+        if not check_password_hash(current_user.password, form.current_password.data):
+            form.current_password.errors.append('Incorrect current password')
+        else:
+            current_user.password = generate_password_hash(form.new_password.data)
+            with sync_session() as session: 
+                session.add(current_user)
+                session.commit()
+            flash('Password changed successfully. Please log in again.')
+            logout_user()
+            return redirect(url_for('admin.login'))
+
+    return render_template('change_password.html', form=form)
+    
